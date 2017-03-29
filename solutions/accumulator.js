28bd23a8a2ec59e0bb18369470e511b1f0f6e559
David Ra Youssef <davidradeveloper@gmail.com>
2017-03-29 13:28:02 -0400

    Edit description

--- solutions/accumulator.js
+++ solutions/accumulator.js
@@ -1,12 +1,18 @@
-// Write a function accumulator() that gives you these results...
+/*
+Write a function accumulator() that gives you these results...
 accumulator("abcd");    // "A-Bb-Ccc-Dddd"
 accumulator("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accumulator("cwAt");    // "C-Ww-Aaa-Tttt"
+*/
 
-function accumulator(s) {
-  return s
+function accumulator(str) {
+  return str
     .split('')
     .map((v,i) => v.repeat(i + 1))
     .map(v => v[0].toUpperCase() + v.slice(1).toLowerCase())
     .join('-');
 }
+
+accumulator('abcd'); //=> "A-Bb-Ccc-Dddd"
+accumulator('RqaEzty'); //=> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accumulator('cwAt'); //=> "C-Ww-Aaa-Tttt"
